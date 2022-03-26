4087738322d6dc4b8f811c2be5c1a9e085f058bf
toginn <89682463+toginn@users.noreply.github.com>
2022-03-26 15:04:31 +0900

    Add: square props

---
+++ src/index.tsx
@@ -0,0 +1,54 @@
+import React, { FC } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+const Square: FC<{ value: number }> = ({ value }) => {
+  return <button className="square">{value}</button>;
+};
+
+const Board = () => {
+  const renderSquare = (i: number) => {
+    return <Square value={i} />;
+  };
+
+  const status = "Next player: X";
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+};
+
+const Game = () => {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <div>{/* status */}</div>
+        <ol>{/* TODO */}</ol>
+      </div>
+    </div>
+  );
+};
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById("root"));

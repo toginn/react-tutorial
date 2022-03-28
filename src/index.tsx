3aebeb120d4b8f2171a4f9cedb51f8a697f7d220
toginn <89682463+toginn@users.noreply.github.com>
2022-03-28 13:28:44 +0900

    Add: helper function

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,26 @@
-import { click } from "@testing-library/user-event/dist/click";
 import React, { FC, useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+
+function calculateWinner(squares: string[]) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 
 const Square: FC<{ v: string; click: () => void }> = ({ v, click }) => {
   return (
